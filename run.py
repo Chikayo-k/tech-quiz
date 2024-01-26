796cce19bc070948cb1082f8769f9dec800677d6
Chika <chika000chika@gmail.com>
2024-01-26 17:48:10 +0000

    Add function to give users to select an easy play mode or hard play mode

--- run.py
+++ run.py
@@ -20,8 +20,33 @@ SHEET =GSPREAD_CLIENT.open("tech_quiz")
 # data = quiz.get_all_values()
 # print(data)
 
-def start_quiz():
-    print("Start quiz")
+def start_easy_game():
+    print("Start easy game!")
+
+def start_hard_game():
+    print("Start hard game!")
+
+def pick_quiz_mode():
+    """
+    Give users to select an easy play mode or hard pay mode
+    """
+    print("\nWould you like to play EASY mode or HARD mode ? \n")
+    print("1. EASY")
+    print("2. Hard")
+
+    count = 0
+    while count <= 0:
+        play_mode = input("Please enter a number 1 or 2")
+
+        if play_mode == "1":
+            start_easy_game()
+            count =+ 1
+        elif play_mode == "2":
+            start_hard_game()
+            count += 1
+        else:
+            print("Input is only valid 1 or 2")
+        
 
 def add_quiz():
     print("Add quiz")
@@ -38,7 +63,7 @@ def select_menu():
         num_selection = input("Please enter a number between 1 and 3 : ")           
         if num_selection  == "1":
             count += 1
-            start_quiz()
+            pick_quiz_mode()
         elif num_selection == "2":
             add_quiz()
             count += 1  
